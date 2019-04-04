80c7c6e1cf5a4b1b69b44c5ef7b2874b39f270a3
Patricia Bonaldy <bonaldypatricia@gmail.com>
2019-04-04 11:05:22 -0300

    commit inicial

---
+++ src/main/java/org/terracotta/jsr107/PersonService.java
@@ -0,0 +1,73 @@
+package org.terracotta.jsr107;
+
+import java.util.Date;
+import java.util.List;
+
+import static java.util.concurrent.TimeUnit.SECONDS;
+
+import java.util.ArrayList;
+
+import javax.cache.CacheManager;
+import javax.cache.annotation.CacheDefaults;
+import javax.cache.annotation.CacheResult;
+import javax.cache.configuration.MutableConfiguration;
+import javax.cache.expiry.Duration;
+import javax.cache.expiry.TouchedExpiryPolicy;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.autoconfigure.cache.JCacheManagerCustomizer;
+import org.springframework.stereotype.Component;
+
+/**
+ *
+ * @author GGIB
+ */
+
+@Component
+@CacheDefaults(cacheName = "people")
+public class PersonService
+{
+    private static final Logger LOGGER = LoggerFactory.getLogger(PersonService.class);
+
+    //create cache
+    @Component
+    public static class CachingSetup implements JCacheManagerCustomizer
+    {
+      @Override
+      public void customize(CacheManager cacheManager)
+      {
+        cacheManager.createCache("people", new MutableConfiguration<>()
+          .setExpiryPolicyFactory(TouchedExpiryPolicy.factoryOf(new Duration(SECONDS, 10)))
+          .setStoreByValue(false)
+          .setStatisticsEnabled(true));
+      }
+    }
+
+    @CacheResult
+    public Person getPerson(int ssn)
+    {
+        LOGGER.info("ssn " + ssn + " not found in cache. TimeStamp: {}", new Date());
+
+        switch (ssn)
+        {
+            case 123456789:
+                return new Person(ssn, "Geoff", "Gibson");
+            case 987654321:
+                return new Person(ssn, "Cory", "Beck");
+            default:
+                return new Person(ssn,"John","Doe");
+        }
+    }
+
+    @CacheResult
+	public List<String> getListPerson() {
+		// TODO Auto-generated method stub
+		List<String> listPersons = new ArrayList<>();
+		listPersons.add("Geoff");
+		listPersons.add("Cory");
+		listPersons.add("John");
+		return listPersons;
+	}
+
+
+}
